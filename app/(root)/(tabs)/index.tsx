4bcafda95dd2723525f5fa3d35bc1be6071f2448
chiku <codexchiku@gmail.com>
2026-05-07 08:00:44 +0530

    initialization and setup of the project completed

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+      <Text className='text-7xl'>welcome every one</Text>
+    </View>
+  )
+}
+
+export default index
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import "@/global.css";
+import { useAuth } from "@clerk/expo";
+import { Redirect } from "expo-router";
+
+export default function Index() {
+  const {isSignedIn,isLoaded} = useAuth()
+
+  if (!isLoaded) return null
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)"/>
+  }
+
+  return <Redirect href="/(auth)/signIn"/>
+}
